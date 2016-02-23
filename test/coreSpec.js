007dba743991dc0de0103ab3495652bf2179b594
Joaquim Neto <jsn.piata@gmail.com>
2016-02-23 14:00:06 -0300

    added support  for request from domain with www.

--- test/coreSpec.js
+++ test/coreSpec.js
@@ -3,8 +3,8 @@
 var core   = require('./../lib/core');
 
 describe("Testing application of core functions", function () {
-  it("Domain 'example.com' in core should be return property 'domain' and 'origin' equals 'example'", function () {
-    var parsedHost = core.parseHost('example.com');
+  it("Domain 'www.example.com' in core should be return property 'domain' and 'origin' equals 'example'", function () {
+    var parsedHost = core.parseHost('www.example.com');
     parsedHost.domain.should.be.equal('example');
     parsedHost.origin.should.be.equal('example');
   });
@@ -27,7 +27,7 @@ describe("Testing application of core functions", function () {
     parsedHost.origin.should.be.equal('api');
   });
 
-  it("When domain equals 'example' and origin equals 'aws', target should be 'http://aws.amazon.com:80'", function () {
+  it("When origin equals 'aws', target should be 'http://aws.amazon.com:80'", function () {
     var target = core.findTarget('example', 'aws');
     target.should.be.equal('http://aws.amazon.com:80');
   });
